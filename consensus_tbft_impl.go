1e0363a406825da9aae120430c3908d2b155eb43
fixed <1185792787@qq.com>
2023-12-25 19:48:50 +0800

    log_analyze liveness

--- consensus_tbft_impl.go
+++ consensus_tbft_impl.go
@@ -676,20 +676,6 @@ func (consensus *ConsensusTBFTImpl) extractProposeOptimal(value string) (propose
 func (consensus *ConsensusTBFTImpl) handle() {
 	consensus.logger.Infof("[%s] handle start", consensus.Id)
 	defer consensus.logger.Infof("[%s] handle end", consensus.Id)
-	// 开始检查共识日志
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			consensus.logger.Infof("fuzzing: ReadSystemLog...")
-			for i := 2; i <= 4; i = i + 1 {
-				err := ReadSystemLog(i, consensus.logger)
-				if err != nil {
-					consensus.logger.Errorf("fuzzing: LogReader failed:" + err.Error())
-					return
-				}
-			}
-		}
-	}()
 	loop := true
 	for loop {
 		// fmt.Println("State_test:", consensus.ConsensusState.Step)
@@ -716,6 +702,15 @@ func (consensus *ConsensusTBFTImpl) handle() {
 			consensus.handleProposeOptimalTimeout()
 		case <-consensus.closeC:
 			loop = false
+		}
+		// 开始检查共识日志
+		consensus.logger.Infof("fuzzing: ReadSystemLog...")
+		for i := 2; i <= 4; i = i + 1 {
+			err := ReadSystemLog(i, consensus.logger)
+			if err != nil {
+				consensus.logger.Errorf("fuzzing: LogReader failed:" + err.Error())
+				return
+			}
 		}
 	}
 }
